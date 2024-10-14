49189d0054f7316d9b177de55e5e1fa3c820c6ca
Yuriy <russian_town_98@mail.ru>
2024-10-14 17:56:51 +0600

    error

---
+++ src/KnowledgelsPower/Assets/CodeBase/Infrastructure/AssetManagement/AssetPath.cs
@@ -0,0 +1,8 @@
+﻿namespace CodeBase.Infrastructure.AssetManagement
+{
+    public static class AssetPath
+    {
+        public const string HudPath = "Hud/Hud";
+        public const string HeroPath = "Hero/hero";
+    }
+}
