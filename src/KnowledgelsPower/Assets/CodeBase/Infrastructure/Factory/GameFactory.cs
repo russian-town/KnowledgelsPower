902c0ea0b5ddd9c33eb0b57b718a73807564a6e8
Yuriy <russian_town_98@mail.ru>
2024-10-14 17:56:51 +0600

    error

---
+++ src/KnowledgelsPower/Assets/CodeBase/Infrastructure/Factory/GameFactory.cs
@@ -0,0 +1,21 @@
+﻿using CodeBase.Infrastructure.AssetManagement;
+using UnityEngine;
+
+namespace CodeBase.Infrastructure.Factory
+{
+    public class GameFactory : IGameFactory
+    {
+        private readonly IAssets _assets;
+
+        public GameFactory(IAssets assets)
+        {
+            _assets = assets;
+        }
+        
+        public GameObject CreateHero(GameObject initialPoint) =>
+            _assets.Instantiate(AssetPath.HeroPath, at: initialPoint.transform.position);
+
+        public void CreateHud() =>
+            _assets.Instantiate(AssetPath.HudPath);
+    }
+}
