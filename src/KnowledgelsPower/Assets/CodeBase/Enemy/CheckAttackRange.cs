3ab549f3de1b3ef70280f21d2000ca739d53f7ff
Yuriy <russian_town_98@mail.ru>
2024-10-16 00:04:39 +0600

    Added_Attack

---
+++ src/KnowledgelsPower/Assets/CodeBase/Enemy/CheckAttackRange.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace CodeBase.Enemy
+{
+    [RequireComponent(typeof(Attack))]
+    public class CheckAttackRange : MonoBehaviour
+    {
+        public Attack Attack;
+        public TriggerObserver TriggerObserver;
+
+        private void Start()
+        {
+            TriggerObserver.TriggerEnter += OnTriggerEnter;
+            TriggerObserver.TriggerExit += OnTriggerExit;
+
+            Attack.DisableAttack();
+        }
+
+        private void OnTriggerEnter(Collider obj)
+        {
+            Attack.EnableAttack();
+        }
+
+        private void OnTriggerExit(Collider obj)
+        {
+            Attack.DisableAttack();
+        }
+
+        private void OnDestroy()
+        {
+            TriggerObserver.TriggerEnter -= OnTriggerEnter;
+            TriggerObserver.TriggerExit -= OnTriggerExit;
+        }
+    }
+}
