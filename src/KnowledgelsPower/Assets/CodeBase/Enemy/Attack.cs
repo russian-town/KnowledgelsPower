855de3a9da33dce51652f77cc51f5fb9e04b905f
Yuriy <russian_town_98@mail.ru>
2024-10-16 00:04:39 +0600

    Added_Attack

---
+++ src/KnowledgelsPower/Assets/CodeBase/Enemy/Attack.cs
@@ -0,0 +1,99 @@
+﻿using System.Linq;
+using CodeBase.Infrastructure.Factory;
+using CodeBase.Infrastructure.Services;
+using UnityEngine;
+
+namespace CodeBase.Enemy
+{
+    [RequireComponent(typeof(EnemyAnimator))]
+    public class Attack : MonoBehaviour
+    {
+        private const string PlayerLayerName = "Player";
+
+        public EnemyAnimator Animator;
+        public float AttackCooldown = 3f;
+        public float Cleavage = .5f;
+        public float EffectiveDistance = .5f;
+
+        private IGameFactory _factory;
+        private Transform _heroTransform;
+        private float _attackCooldown;
+        private bool _isAttacking;
+        private int _layerMask;
+        private Collider[] _hits = new Collider[1];
+        private bool _attackIsActive;
+
+        private void Awake()
+        {
+            _factory = AllServices.Container.Single<IGameFactory>();
+            _layerMask = 1 << LayerMask.NameToLayer(PlayerLayerName);
+            _factory.HeroCreated += OnHeroCreated;
+        }
+
+        private void Update()
+        {
+            UpdateCooldown();
+
+            if (CanAttack())
+                StartAttack();
+        }
+
+        private void OnAttack()
+        {
+            if (Hit(out Collider hit))
+            {
+                PhysicsDebug.DrawDebug(StartPoint(), Cleavage, 1f);
+            }
+        }
+
+        private bool Hit(out Collider hit)
+        {
+            var hitsCount = Physics.OverlapSphereNonAlloc(StartPoint(), Cleavage, _hits, _layerMask);
+            hit = _hits.FirstOrDefault();
+            return hitsCount > 0;
+        }
+
+        private Vector3 StartPoint() =>
+            new Vector3(transform.position.x, transform.position.y + .5f, transform.position.z)
+            + transform.forward
+            * EffectiveDistance;
+
+
+        private void OnAttackEnded()
+        {
+            _attackCooldown = AttackCooldown;
+            _isAttacking = false;
+        }
+
+        public void EnableAttack() => 
+            _attackIsActive = true;
+
+        public void DisableAttack() => 
+            _attackIsActive = false;
+
+        private void StartAttack()
+        {
+            transform.LookAt(_heroTransform);
+            Animator.PlayAttack();
+            _isAttacking = true;
+        }
+
+        private void UpdateCooldown()
+        {
+            if (!CooldownIsUp())
+                _attackCooldown -= Time.deltaTime;
+        }
+
+        private bool CanAttack() =>
+            _attackIsActive && !_isAttacking && CooldownIsUp();
+
+        private bool CooldownIsUp() =>
+            _attackCooldown <= 0f;
+
+        private void OnHeroCreated()
+        {
+            _heroTransform = _factory.HeroGameObject.transform;
+            _factory.HeroCreated -= OnHeroCreated;
+        }
+    }
+}
